e91d889cec95cf448062dc6a4d39b981c22632ba
guiqi.hu@163.com <qiguihu198838>
2017-10-27 17:45:51 +0800

    add activiti

---
+++ src/main/java/com/example/demo/OnboardingRequest.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+/**
+ * Created by guiqi on 2017/10/26.
+ */
+public class OnboardingRequest {
+}
---
+++ src/test/java/com/example/demo/VacationRequestTest.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import org.activiti.engine.*;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by guiqi on 2017/10/27.
+ */
+
+public class OnboardingRequestTest {
+
+    @Before
+    public void init(){
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:mysql://localhost:3306/activiti?useUnicode=true&characterEncoding=utf-8")
+                .setJdbcUsername("root")
+                .setJdbcPassword("123456")
+                .setJdbcDriver("com.mysql.jdbc.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        String pName = processEngine.getName();
+        String ver = ProcessEngine.VERSION;
+        System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+    }
+
+    @Test
+    public void testDeployProcessEngine(){
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment()
+                .addClasspathResource("VacationRequest.bpmn20.xml")
+                .deploy();
+
+        System.out.println("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());
+    }
+
+    @Test
+    public void testRequestVocation(){
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("employeeName", "Kermit");
+        variables.put("numberOfDays", new Integer(4));
+        variables.put("vacationMotivation", "I'm really tired!");
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("vacationRequest", variables);
+
+// Verify that we started a new process instance
+        System.out.println("Number of process instances: " + runtimeService.createProcessInstanceQuery().count());
+    }
+
+
+    @Test
+    public void testListTask(){
+        // Fetch all tasks for the management group
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("management").list();
+        for (Task task : tasks) {
+            System.out.println("Task Id: " + task.getId());
+            System.out.println("Task available: " + task.getName());
+        }
+    }
+}
