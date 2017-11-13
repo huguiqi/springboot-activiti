3e8bdbd589151ca353e9bd7459f657c45c6b6dc0
huguiqi <guiqi.hu@163.com>
2017-11-13 17:47:58 +0800

    integration oracle

---
+++ src/main/java/com/example/demo/service/OneTaskService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by guiqi on 2017/11/13.
+ */
+
+@Service
+public class OneTaskService {
+
+    public void printNewTask(){
+
+        System.out.println("printNewTask");
+    }
+}
